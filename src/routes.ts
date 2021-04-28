183a77f9c02ec326db5db1bd8c1e89e697ee3ef4
MauroBraga <M@r202150016774>
2021-04-28 08:45:58 -0300

    Adicionando os tipos

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { Request, Response, response } from 'express';
+import CreateCourseService from './CreateCourseService';
+
+export  function createCourse(request: Request, response: Response){
+    CreateCourseService.execute({
+        name:"NodeJS", 
+        duration:10, 
+        educator:"Dani"});
+    return response.send();
+}
